178f5fcce0596683dcd8805873e0c1ea8789f94a
unknown <simranjaiswal2530@gmail.com>
2022-08-05 13:34:48 +0530

    initial

---
+++ qr scanner/script.js
@@ -0,0 +1,30 @@
+function genQR(){
+    var gapi="https://chart.googleapis.com/chart?chf=bg,s,65432100&cht=qr&chs=" ;
+    var myimg = document.querySelector("#img");
+    var mytext=document.querySelector("#qrtext").value;
+    var mysize=document.querySelector(".size").value;
+    
+    if(mytext!==" "&& mysize=="100")
+    {
+        myimg.src= gapi+"100x100"+"&chl="+mytext;
+    }
+    else if(mytext!==""&& mysize=="150")
+    {
+        myimg.src= gapi+"150x150"+"&chl="+mytext;
+    }
+    else if(mytext!==""&& mysize=="200")
+    {
+        myimg.src= gapi+"200x200"+"&chl="+mytext;
+    }
+    else if(mytext!==""&& mysize=="250")
+    {
+        myimg.src= gapi+"250x250"+"&chl="+mytext;
+    }
+    else if(mytext!==""&& mysize=="300")
+    {
+        myimg.src= gapi+"300x300"+"&chl="+mytext;
+    }
+    else{
+        alert("Enter Text");
+    }
+}
